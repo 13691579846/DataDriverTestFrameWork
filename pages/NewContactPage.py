97687cd9eee11482243f1389f24b0f7b6e7c4aed
13691579846 <281754043@qq.com>
2019-08-04 00:18:06 +0800

    modify by linuxchao:由于126邮箱升级,修改了部分代码

--- pages/NewContactPage.py
+++ pages/NewContactPage.py
@@ -1,3 +1,14 @@
+"""
+------------------------------------
+@Time : 2019/8/3 21:59
+@Auth : linux超
+@File : GetDateTime.py
+@IDE  : PyCharm
+@Motto: Real warriors,dare to face the bleak warning,dare to face the incisive error!
+@QQ   : 28174043@qq.com
+@GROUP: 878565760
+------------------------------------
+"""
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import TimeoutException
--- util/RecordLog.py
+++ util/RecordLog.py
@@ -1,3 +1,14 @@
+"""
+------------------------------------
+@Time : 2019/8/3 21:59
+@Auth : linux超
+@File : GetDateTime.py
+@IDE  : PyCharm
+@Motto: Real warriors,dare to face the bleak warning,dare to face the incisive error!
+@QQ   : 28174043@qq.com
+@GROUP: 878565760
+------------------------------------
+"""
 import logging
 import time
 
